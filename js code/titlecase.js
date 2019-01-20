a417e4e93e83b64ac8f929fde341cb98484d3eef
Danny Thompson <dannythompson901@gmail.com>
2019-01-19 23:18:10 -0600

    Create titlecase.js

---
+++ js code/titlecase.js
@@ -0,0 +1,10 @@
+function titleCase(str) {
+  str = str.toLowerCase().split(" ");
+  var str1 = str.map( function(val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  
+  str1 = str1.join(" ");
+  
+  return str1;
+}
